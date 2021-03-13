0a95eae2469588f8e4bcb2254d661833993a6758
Takato Okudo <okudo@nii.ac.jp>
2021-03-13 15:30:23 +0900

    [feat] change directory structures.

--- learning/pinball/main.py
+++ learning/pinball/main.py
@@ -172,7 +172,6 @@ def learning_loop(args):
     max_q_episode_list = []
     runtimes = []
     max_q = 0.0
-    saved_dir = "data"
     start_time = time.time()
     for i in range(episode_count):
         total_reward = 0
@@ -211,42 +210,32 @@ def learning_loop(args):
                 vis.set_action_dist(agent.vis_action_dist, action)
                 vis.pause(.0001)
         max_q_episode_list.append(max_q)
-        saved_model_dir = os.path.join(saved_dir, 'model')
-        agent.save_model(saved_model_dir, i)
+        agent.save_model(d_kinds["mo"], i)
     # export process
     runtimes.append(time.time() - start_time)
-    saved_res_dir = os.path.join(saved_dir, 'res')
-    export_csv(saved_res_dir, fnames['total_reward'], total_reward_list)
+    export_csv(
+        d_kinds['tr'],
+        fnames['total_reward'],
+        total_reward_list
+    )
     td_error_list = agent.td_error_list
-    export_csv(saved_res_dir, fnames['td_error'], td_error_list)
+    export_csv(
+        d_kinds["td"],
+        fnames['td_error'],
+        td_error_list
+    )
     total_reward_list = np.array(total_reward_list)
     steps_list = np.array(steps_list)
     max_q_list = np.array(max_q_list)
     logger.debug("Average return: {}".format(np.average(total_reward_list)))
-    steps_file_path = os.path.join(saved_res_dir, fnames['steps'])
+    steps_file_path = os.path.join(d_kinds["st"], fnames['steps'])
     pd.DataFrame(steps_list).to_csv(steps_file_path)
-    runtime_file_path = os.path.join(saved_res_dir, fnames['runtime'])
+    runtime_file_path = os.path.join(d_kinds["ru"], fnames['runtime'])
     runtimes_df = pd.DataFrame(runtimes, columns=["runtime"]).to_csv(runtime_file_path)
-    # save model
-    # saved_model_dir = os.path.join(saved_dir, 'model')
-    # agent.save_model(saved_model_dir)
     env.close()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Actor-Critic Learning.')
-    parser.add_argument('env_id', nargs='?', default='Pinball-Subgoal-v0', help='Select the environment to run.')
-    parser.add_argument('--vis', action='store_true', help='Attach when you want to look visual results.')
-    parser.add_argument('--model', help='Input model dir path')
-    parser.add_argument('--nepisodes', default=250, type=int)
-    parser.add_argument('--nruns', default=25, type=int)
-    parser.add_argument('--id') # , choices=ALG_CHOICES
-    parser.add_argument('--subg-path', default='', type=str)
-    parser.add_argument('--eta', default=10000, type=int)
-    parser.add_argument('--rho', default=0.01, type=int)
-    parser.add_argument('--k', type=int, help='How many to discretize the observation related to space.')
-    
-    args = parser.parse_args()
     learning_time = time.time()
     if "srs" in args.id:
         subg_serieses = load_subgoals_new(args.subg_path)
@@ -268,13 +257,38 @@ def main():
         ]
         with ProcessPoolExecutor() as executor:
             tqdm(executor.map(learning_loop, arguments), total=args.nruns)
+        
+        # Single Process
         # for run in range(args.nruns):
         #     logger.debug(f"Run: {run+1}/{args.nruns}")
-        #     learning_loop(run, args.env_id, args.nepisodes, args.model,
-        #                     args.vis, args.id, args.rho, args.eta, subg_series, l_id, args.k)
+        #     learning_loop(arguments[run])
         #     # Close the env and write monitor result info to disk
     duration = time.time() - learning_time
     logger.info("Learning time: {}m {}s".format(int(duration//60), int(duration%60)))
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Actor-Critic Learning.')
+    parser.add_argument('env_id', nargs='?', default='Pinball-Subgoal-v0', help='Select the environment to run.')
+    parser.add_argument('--vis', action='store_true', help='Attach when you want to look visual results.')
+    parser.add_argument('--model', help='Input model dir path')
+    parser.add_argument('--nepisodes', default=250, type=int)
+    parser.add_argument('--nruns', default=25, type=int)
+    parser.add_argument('--id') # , choices=ALG_CHOICES
+    parser.add_argument('--subg-path', default='', type=str)
+    parser.add_argument('--eta', default=10000, type=float)
+    parser.add_argument('--rho', default=0.01, type=float)
+    parser.add_argument('--k', type=int, help='How many to discretize the observation related to space.')
+    
+    args = parser.parse_args()
+    saved_dir = os.path.join("out", args.id)
+    d_kinds = {
+        "tr": os.path.join(saved_dir, "total_reward"),
+        "st": os.path.join(saved_dir, "steps"),
+        "td": os.path.join(saved_dir, "td_error"),
+        "mo": os.path.join(saved_dir, "model"),
+        "ru": os.path.join(saved_dir, "runtime")
+    }
+    for fpath in d_kinds.values():
+        if not os.path.exists(fpath):
+            os.makedirs(fpath)
     main()
--- learning/pinball/run.sh
+++ learning/pinball/run.sh
@@ -1,6 +1,12 @@
-# python main.py --id='srs' --nruns=100 --nepisodes=200 --subg-path="in/subgoals/human_subgoals.csv"
-python main.py --id='sarsa-rs-jk3' --nruns=100 --nepisodes=200 --k=3
+for ETA in 100
+do
+echo "human-srs-eta=${ETA}"
+python main.py --id="human-srs-eta=${ETA}" --nruns=100 --nepisodes=200 --subg-path="in/subgoals/human_subgoals.csv" --eta=$ETA --rho=0
+echo "random-srs-eta=${ETA}"
+python main.py --id="random-srs-eta=${ETA}" --nruns=100 --nepisodes=200 --subg-path="in/subgoals/random_subgoals.csv" --eta=$ETA --rho=0
+done
+# python main.py --id='sarsa-rs-jk3' --nruns=100 --nepisodes=200 --k=3
 # python main.py --id="actor-critic" --nruns=100 --nepisodes=200
 # python main.py --id="online-subgoal-human" --nruns=10 --nepisodes=200 --subg-path="in/subgoals/human_subgoals.csv"
 # python main.py --id="naive-subgoal-human" --nruns=10 --nepisodes=200 --subg-path="in/subgoals/human_subgoals.csv"
-# python main.py --id="online-subgoal-radom" --nruns=10 --nepisodes=200 --subg-path="in/subgoals/random_subgoals.csv"
+# python main.py --id="online-subgoal-random" --nruns=10 --nepisodes=200 --subg-path="in/subgoals/random_subgoals.csv"
