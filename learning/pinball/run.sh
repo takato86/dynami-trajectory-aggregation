90c888a5ecc04a23edc5b2fa5bf0c2efb8df136b
takato86 <okudo@nii.ac.jp>
2022-11-15 22:46:43 +0900

    add pinball configs.

--- learning/pinball/run.sh
+++ learning/pinball/run.sh
@@ -1,4 +1,4 @@
-for JSON in "dta.json" "linrs.json" "nrs.json" "srs.json"
+for JSON in "srs.json" "static_random.json"
 do
 echo "in/configs/${JSON}"
 xvfb-run -a python main.py --config="in/configs/${JSON}"
